c403c1a871b1a0393a25fc08809a6da8b66344c2
doglink <iramaxpro@gmail.com>
2021-04-22 14:44:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function throttle(func, delay) {
 }
 
 const cb = throttle(evt => {
-  defineWhichItem(window.scrollY - 0)
+  defineWhichItem(window.scrollY - calc((1.5em + 0.375 * ((100vw - 20em) / 42.5)) * 12)
 }, 0)
 
 window.addEventListener('scroll', cb)
