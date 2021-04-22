1c163f53e09c39e25af4f51fb88cc8c315c40a23
doglink <iramaxpro@gmail.com>
2021-04-22 14:36:45 +0300

    hazo

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-function playAudio(url) {
-  new Audio(url).play();
+const items = Array.from(document.querySelectorAll('.list-item'))
+
+function throttle(func, delay) {
+  let timeout = null
+  return function(...args) {
+    if (!timeout) {
+      timeout = setTimeout(() => {
+        func.call(this, ...args)
+        timeout = null
+      }, delay)
+    }
+  }
 }
+
+const cb = throttle(evt => {
+  defineWhichItem(window.scrollY - 0)
+}, 0)
+
+window.addEventListener('scroll', cb)
+
+const playAudio = (fileName) => {
+  const audio = new Audio(`../${fileName}.mp3`);
+  audio.play();
+}
+
+const defineWhichItem = (scrollY) => {
+  items.find(item => {
+    const elemRect = item.getBoundingClientRect()
+    const bodyRect = document.body.getBoundingClientRect()
+    const offset = elemRect.top - bodyRect.top
+    if (offset > scrollY && offset <= scrollY + elemRect.height) {
+      item.classList.add('highlighted')
+      const trackName = item.getAttribute('data-audio')
+      playAudio(trackName)
+    } else {
+      item.classList.remove('highlighted')
+    }
+  })
+}
+
